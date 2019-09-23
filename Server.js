d32f5561edea1c89f24dfac206f2d61d4cb8a88f
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-09-23 08:39:53 +0000

    message

---
+++ Server.js
@@ -0,0 +1,6 @@
+const JS = require('shelljs').ls("Angular/dist/demo/*.js")
+    .reduce((accum, cur) => accum + `<script src="https://www.hwangsehyun.ga/${cur}"></script>\n`, "");
+console.log("Angular.html");
+console.log(JS);
+
+require("fs").writeFile('Angular/index.html', JS, 'utf8', Function.prototype);
