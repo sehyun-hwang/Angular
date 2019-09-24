5a3dadf5d3268375ca36490f5d25780d029af99a
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-09-24 16:54:06 +0900

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './material-module';
 
 import { CountdownModule } from 'ngx-countdown';
 import { WebcamModule } from 'ngx-webcam';
 
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent}from "./app.component";
-
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 import { TeachersDay } from './TeachersDay.component';
 import { StreetCapture } from './StreetCapture.component';
@@ -31,16 +26,12 @@ const routes: Routes  = [
   declarations: [...routes.map(x=>x.component), AppComponent],
   imports: [
     BrowserModule,
-    FormsModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-
-    MatTabsModule,
-    MatButtonModule,
-    MatTableModule,
-    MatSortModule,
-  MatAutocompleteModule,
 
     CountdownModule,
     WebcamModule,
