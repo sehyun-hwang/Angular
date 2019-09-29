fa1afaed46799f8781d68e3e6c65e5d205d4243a
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-09-29 13:16:31 +0000

    message

--- src/app-/app.module.ts
+++ src/app-/app.module.ts
@@ -10,4 +10,4 @@ import { HelloComponent } from './hello.component';
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
