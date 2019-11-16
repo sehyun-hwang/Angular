6b014bc835279ddeb40a734e59a8e695439cfe9a
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-11-16 12:15:41 +0000

    message

--- dist.sh
+++ dist.sh
@@ -1,6 +1,8 @@
 if [ "$1" == "update" ]; then
     ng update @angular/cli @angular/core --allow-dirty
 fi
-ng build
+echo Building
+#ng build
 node dist.js
+cat src/index.html >> dist/demo/index.html
 aws s3 sync --acl public-read dist/demo s3://hwangsehyun/Angular
