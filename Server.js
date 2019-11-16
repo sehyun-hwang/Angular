7e7e0dedbd51d74694354d4916e624d3bb1b66f4
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-11-16 11:23:44 +0000

    message

--- Server.js
+++
@@ -1,6 +0,0 @@
-const HTML = require('shelljs').ls("Angular/dist/demo/*.js")
-    .reduce((accum, cur) => accum + `<script src="https://www.hwangsehyun.ga/${cur}"></script>\n`, "");
-console.log("Angular.html");
-console.log(HTML);
-
-require("fs").writeFile('Angular/index.html', HTML, Function.prototype);
--- dist.sh
+++ dist.sh
@@ -1,4 +1,6 @@
-if [ "$1" != "update" ]; then
+if [ "$1" == "update" ]; then
     ng update @angular/cli @angular/core --allow-dirty
 fi
 ng build
+node dist.js
+aws s3 sync --acl public-read dist/demo s3://hwangsehyun/Angular
