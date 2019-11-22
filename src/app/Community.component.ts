61bded7bba510eda7bdf2231da1683c8bf2dec0a
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-11-22 13:27:17 +0900

    1 file modified, 2 added

---
+++ src/app/Community.component.ts
@@ -0,0 +1,10 @@
+import {Component } from '@angular/core';
+
+@Component({
+  selector: '',
+  templateUrl: './Community.component.html',
+  //styleUrls: ['./Community.component.css'],
+})
+export class Community {
+
+}
--- src/app/Routes.ts
+++ src/app/Routes.ts
@@ -4,10 +4,12 @@ import {Index} from "./index.component"
 import { TeachersDay } from './TeachersDay.component';
 import { StreetCapture } from './StreetCapture.component';
 import { PnID } from './PnID.component';
+import { Community } from './Community.component';
 
 export const routes: Routes  = [
   { path: '', component: Index },
   { path: 'Angualr', component: Index },
+  { path: "community", component: Community },
   { path: 'teachersday/console.html', component: TeachersDay },
   { path: 'ptais/street-capture.html', component: StreetCapture },
   { path: 'pnid', component: PnID },
