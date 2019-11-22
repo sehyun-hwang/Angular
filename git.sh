8f25bd7ff5593bb682b8c28aa34172a374076a9e
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-11-22 13:13:36 +0900

    message

--- git.sh
+++ git.sh
@@ -1,3 +1,19 @@
 rm report*.json
-git pull origin master
-git add . ; git commit -m "message"; git push;
+
+for x in "$@"
+do
+    echo "$x"
+
+    if [ "$x" != "pull" ]; then
+        git pull origin master
+        exit
+    fi
+
+    if [ "$x" != "commit" ]; then
+        git add .
+        git commit -m "message"
+        git push
+    fi
+done
+
+
