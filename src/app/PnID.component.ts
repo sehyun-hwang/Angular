ddffdfdbadbf40d273bdeb9deb749b9e1331aafc
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-11-25 09:52:11 +0100

    2 files modified

--- src/app/PnID.component.ts
+++ src/app/PnID.component.ts
@@ -1,23 +1,26 @@
-import {Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import {FormControl} from '@angular/forms';
-import {MatListModule} from '@angular/material/list';
+import { FormControl } from "@angular/forms";
+import { MatListModule } from "@angular/material/list";
 
-import { SocketIO } from "./modules/socket.io"
-import { Observable, Subject } from 'rxjs';
+import { SocketIO } from "./modules/socket.io";
+import { Observable, Subject } from "rxjs";
 
 @Component({
-  selector: '',
-  templateUrl: './PnID.component.html',
-  styleUrls: ['./PnID.component.css'],
+  selector: "",
+  templateUrl: "./PnID.component.html",
+  styleUrls: ["./PnID.component.css"]
 })
 export class PnID {
-  table:Object;
+  table: Object;
+  displayedColumns: string[] = ['id', 'name'];
   messages: Subject<any>;
   constructor(private socketIO: SocketIO) {
-    fetch("https://jsonplaceholder.typicode.com/users").then(data=>data.json())
-    .then(data=>this.table=data);
-     this.messages = <Subject<any>>this.socketIO.connect();
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(data => data.json())
+      .then(data => this.table = data)
+      .then(console.log);
+    this.messages = <Subject<any>>this.socketIO.connect();
   }
   myControl = new FormControl();
   test() {
