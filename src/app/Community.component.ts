77e7a0f652e84877a91ef1c2d5f8307de4e6c5fa
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-11-26 21:41:30 +0100

    1 file modified

--- src/app/Community.component.ts
+++ src/app/Community.component.ts
@@ -1,15 +1,15 @@
-import {Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: '',
-  templateUrl: './Community.component.html',
-  styleUrls: ['./Community.component.css'],
+  selector: "",
+  templateUrl: "./Community.component.html",
+  styleUrls: ["./Community.component.css"]
 })
 export class Community {
   Form_Initial = {
-    dong: [...Array(7)].map((x,i)=>i+101),
-    floor: (()=>{
+    dong: [...Array(7)].map((x, i) => i + 101),
+    floor: (() => {
       const arr = [...Array(10)];
       arr.pop();
       return arr;
@@ -17,17 +17,23 @@ export class Community {
   };
 
   Form = {
-    Mode:"User",
+    Mode: "User",
     dong: 101,
-    floor: 1,
+    floor: 1
   };
 
+  Submit() {
+    console.log(this.Form);
+  }
 
-  Devices = [{
-    EN: "humidifier",
-    KR: "가습기" 
-  }, {
-    EN: "light",
-    KR: "무드등" 
-  }];
+  Devices = [
+    {
+      EN: "humidifier",
+      KR: "가습기"
+    },
+    {
+      EN: "light",
+      KR: "무드등"
+    }
+  ];
 }
