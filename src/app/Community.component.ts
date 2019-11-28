6ff42dd2824474c346b9c84cfd2007977e576c4c
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-11-28 19:57:22 +0100

    3 files modified

--- src/app/Community.component.ts
+++ src/app/Community.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: "",
@@ -7,17 +8,42 @@ import { FormControl } from "@angular/forms";
   styleUrls: ["./Community.component.css"]
 })
 export class Community implements OnInit {
+  constructor(private sanitizer: DomSanitizer) {}
+  public Santize(url: string) {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
   Form_Initial = {
-    dong: Array.from({length: 7}, (x, i) => i+101),
-    floor: Array.from({length: 10}, (x, i) => i+1)
+    dong: Array.from({ length: 7 }, (x, i) => i + 101),
+    floor: Array.from({ length: 10 }, (x, i) => i + 1)
   };
 
   Tab = new FormControl(0);
+  src: string;
+  data: object;
   ngOnInit() {
     this.Tab.valueChanges.subscribe(value => {
       this.Form.Unit = ["d", "m"][this.Tab.value];
       this.Submit();
     });
+
+    const Fetch = fetch("https://www.hwangsehyun.ga/Community/index.svg");
+
+    Fetch.then(data => data.clone().blob()).then(
+      blob => (this.src = URL.createObjectURL(blob))
+    );
+
+    Fetch.then(data => data.text())
+      .then(
+        text =>
+          new DOMParser()
+            .parseFromString(text, "text/xml")
+            .querySelector("json").innerHTML
+      )
+      .then(JSON.parse)
+      .then(text => {
+        console.log(text);
+      });
   }
 
   Form = {
--- src/app/PnID-PlantAsset.ts
+++ src/app/PnID-PlantAsset.ts
@@ -1,5 +1,5 @@
 export async function Parser(promise: Response) {
-  const d = await promise.text()
+  const data = await promise.text()
     .then(
       text => new DOMParser().parseFromString(text, "text/xml").firstElementChild.innerHTML)
     .then(JSON.parse)
