b2a209ef2179268bd8a07c00c39a088d384dad65
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-12-04 08:16:17 +0900

    message

--- dist.sh
+++ dist.sh
@@ -2,9 +2,12 @@ if [ "$1" == "update" ]; then
     ng update @angular/cli @angular/core --allow-dirty
 fi
 
-
 echo Building
+if ! grep -R browserslist package.json; then
+    sed -i '$s/}/\n,"browserslist": [ "> 5%" ]\n}/' package.json
+fi
 ng build
 cp -f index.html dist/demo/index.html
 node dist.js
 aws s3 sync --acl public-read dist/demo s3://hwangsehyun/Angular
+
