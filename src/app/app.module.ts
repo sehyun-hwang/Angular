b3a74e85216469d9f0923eb5bf470be5837c7d14
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-12-11 14:05:29 +0900

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {DemoMaterialModule} from './Material.module';
 import { CountdownModule } from 'ngx-countdown';
 import { WebcamModule } from 'ngx-webcam';
 import { ChartsModule } from 'ng2-charts';
+import { GaugeModule } from 'angular-gauge';
 
 import { RouterModule } from '@angular/router';
 import { AppComponent }from "./app.component";
@@ -38,6 +39,7 @@ AppComponent,  PnID_Device, PnID_Dialog
     CountdownModule,
     WebcamModule,
     ChartsModule,
+    GaugeModule.forRoot(),
 
     RouterModule.forRoot((location.hostname.includes("pnid") || window["cordova"])?
     [{path:"**", component: PnID}]:routes),
