e35fef2e80f881d3ecad7b525019ae92ff9f990b
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-12-11 19:36:57 +0900

    message

--- dist.sh
+++ dist.sh
@@ -7,6 +7,7 @@ if ! grep -R browserslist package.json; then
     sed -i '$s/}/\n,"browserslist": [ "> 5%" ]\n}/' package.json
 fi
 ng build
+set -e
 cp -f index.html dist/demo/index.html
 node dist.js
 aws s3 sync --acl public-read dist/demo s3://hwangsehyun/Angular
