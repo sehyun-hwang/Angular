068d538959e3eee842407774b26c5c734168730d
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-12-17 08:56:25 +0900

    message

--- dist.sh
+++ dist.sh
@@ -3,22 +3,18 @@ if [ "$1" == "update" ]; then
 fi
 
 echo Building
-if ! grep -R browserslist package.json; then
+if ! grep browserslist package.json; then
     sed -i '$s/}/\n,"browserslist": [ "> 5%" ]\n}/' package.json
 fi
 
 set -e
 NODE_OPTIONS="--max-old-space-size=2048" ng build --prod
+aws s3 sync --acl public-read dist/demo s3://hwangsehyun/Angular
 
 URL="https://hwangsehyun.s3-ap-southeast-1.amazonaws.com/Angular/"
-if ! grep -Fxq $URL dist/demo/index.html; then
-    perl -i -pe 's/((?:src)|(?:href))="(?!https)([^"]*)"/$1="'${URL//\//\\\/}'$2"/g' dist/demo/index.html
-fi
-
-cat index.html >> dist/demo/index.html
-aws s3 sync --acl public-read dist/demo s3://hwangsehyun/Angular
-aws s3 cp --acl public-read \
+curl https://proxy.hwangsehyun.ga/angular/index.html | \
+    perl -i -pe 's/((?:src)|(?:href))="(?!https|\/)([^"]*)"/$1="'${URL//\//\\\/}'$2"/g' | \
+    aws s3 cp --acl public-read \
     --content-type "text/html; charset=utf-8" \
     --metadata-directive REPLACE \
-    s3://hwangsehyun/Angular/index.html \
-    s3://hwangsehyun/Angular/index.html 
+    - s3://hwangsehyun/Angular/index.html
