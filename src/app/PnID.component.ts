a7f0c16d6cd35dfc6bc5b6865cf140dcb4c97b09
aegggsehyun1994 <hwanghyun3@gmail.com>
2019-12-23 01:09:29 +0900

    2 files modified

--- src/app/PnID.component.ts
+++ src/app/PnID.component.ts
@@ -12,11 +12,12 @@ import IO from "socket.io-client";
 })
 export class PnID {
   table: Object;
-  displayedColumns: string[] = ["id", "name"];
+  displayedColumns: string[] = ["time", "event"];
 
   io;
   constructor() {
     this.io = IO("https://proxy.hwangsehyun.ga?port=8081");
+  this.io.on("AngularTable", data =>this.table = data);
 
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(data => data.json())
