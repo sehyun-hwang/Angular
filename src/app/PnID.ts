2409501d960ce983b60d7b005d97a783b43b328a
aegggsehyun1994 <hwanghyun3@gmail.com>
2020-02-04 00:40:38 +0900

    1 file modified

--- src/app/PnID.ts
+++ src/app/PnID.ts
@@ -6,28 +6,31 @@ import { IOInterface } from "./PnID-Interfaces";
 
 @Injectable()
 export class IOInjectable extends (io as IOInterface) {
-  Socket:SocketIOClient.Socket;
+  Socket: SocketIOClient.Socket;
 
   constructor() {
-    const Socket = super("https://proxy.hwangsehyun.com?port=8081") as unknown as SocketIOClient.Socket;
-    this.Socket= Socket;
+    const Socket = (super(
+      "https://proxy.hwangsehyun.com?port=8081"
+    ) as unknown) as SocketIOClient.Socket;
+    this.Socket = Socket;
   }
 }
 
-export async function Parser(arg: string):Promise<any[]>
-export async function Parser(arg: Response):Promise<any[]>{
-  const data:  {
-    Table:any[]
-  } = await Promise.resolve(arg instanceof Response? await arg.text() : arg)
-  .then(
+async function Parser(arg: string): Promise<any[]>
+async function Parser(arg: Response): Promise<any[]> {
+  arg
+  const data: {
+    Table: any[];
+  } = await Promise.resolve(arg instanceof Response ? await arg.text() : arg)
+    .then(
       text =>
         new DOMParser().parseFromString(text, "text/xml").firstElementChild
           .innerHTML
-    ).then(JSON.parse);
+    )
+    .then(JSON.parse);
   return Promise.resolve(data.Table);
 }
 
 export function Timestamp(n: number) {
   return new Date(n * 1000).toLocaleString();
 }
-
